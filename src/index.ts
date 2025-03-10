a62c45c036a196aeb333928c0921d3006da15de6
Utkarsh <utkarshjaiswal537@gmail.com>
2025-03-10 06:05:03 +0530

    added cors origin

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
